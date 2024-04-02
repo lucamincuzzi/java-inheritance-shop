3c353fe1ecfb20782aec372bf881a28385f10bef
lucamincuzzi <luca.mincuzzi@outlook.it>
2024-04-02 15:57:53 +0200

    add: getter and setter

--- src/org/experis/learning/Prodotto.java
+++ src/org/experis/learning/Prodotto.java
@@ -1,6 +1,7 @@
 package org.experis.learning;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Random;
 
 public class Prodotto {
@@ -12,19 +13,58 @@ public class Prodotto {
     private BigDecimal iva;
 
     // Costruttori
-
     public Prodotto(String nome, String marca, BigDecimal prezzo, BigDecimal iva) {
         this.codice = generaCodice();
         this.marca = marca;
         this.prezzo = prezzo;
-        this.iva = iva;
+        this.iva = iva.divide(BigDecimal.valueOf(100), RoundingMode.HALF_DOWN);
     }
 
     // Metodi
-
     private int generaCodice(){
         Random rnd = new Random();
 
         return rnd.nextInt(1000) + 1;
+    }
+
+        // Getter e Setter
+    public int getCodice() {
+        return codice;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public BigDecimal getPrezzo() {
+        return prezzo;
+    }
+
+    public BigDecimal getPrezzoCompleto(){
+        return prezzo.add(prezzo.multiply(iva));
+    }
+
+    public void setPrezzo(BigDecimal prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    public BigDecimal getIva() {
+        return iva;
+    }
+
+    public void setIva(BigDecimal iva) {
+        this.iva = iva;
     }
 }
