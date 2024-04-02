9869178172711688aa8ff3e3c8dd719ebec36e85
lucamincuzzi <luca.mincuzzi@outlook.it>
2024-04-02 16:57:57 +0200

    add: Cuffia subclass

---
+++ src/org/experis/learning/Cuffia.java
@@ -0,0 +1,37 @@
+package org.experis.learning;
+
+import java.math.BigDecimal;
+import java.util.Random;
+
+public class Cuffia extends Prodotto{
+    // Attributi
+    private int codiceIMEI;
+    private int memoria;
+
+    // Costruttori
+    public Cuffia(String nome, String marca, BigDecimal prezzo, BigDecimal iva, int codiceIMEI, int memoria) {
+        super(nome, marca, prezzo, iva);
+        this.codiceIMEI = generaIMEI();
+        this.memoria = memoria;
+    }
+
+    // Metodi
+    private int generaIMEI(){
+        Random rnd = new Random();
+
+        return rnd.nextInt(1000000000);
+    }
+
+        // Getter e Setter
+    public int getCodiceIMEI() {
+        return codiceIMEI;
+    }
+
+    public String getMemoria() {
+        return memoria + "GB";
+    }
+
+    public void setMemoria(int memoria) {
+        this.memoria = memoria;
+    }
+}
